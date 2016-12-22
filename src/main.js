cb66f8e8d5dba0eb38865e15ed03331d11a30d01
hadaskoren <das.koren@gmail.com>
2016-12-22 16:11:53 +0200

    conflics solved

--- src/main.js
+++ src/main.js
@@ -7,20 +7,14 @@ import Home from './pages/home/home.vue';
 import About from './pages/about/about.vue';
 import MainNav from './pages/main-nav/main-nav.vue';
 import Places from './pages/places/places-center.vue';
-<<<<<<< HEAD
-Vue.use(VueResource);
-Vue.use(VueRouter);
-
-Vue.http.options.root = 'http://localhost:3003';
-
-
-=======
 import Emails from './pages/email-center/email-center.vue';
 import Events from './pages/event-center/event-center.vue';
 
+Vue.http.options.root = 'http://localhost:3003';
+
+Vue.use(VueResource);
 Vue.use(VueRouter);
 
->>>>>>> 92cb4b5ba120e8a9024468f948675491ed424516
 const routes = [{
     path: '/',
     component: Home
