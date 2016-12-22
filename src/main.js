0a4a1c6cd8118b526735a839773b320695925255
Adirsimona <adirsimona@gmail.com>
2016-12-22 16:15:09 +0200

    updates

--- src/main.js
+++ src/main.js
@@ -6,22 +6,20 @@ import VueResource from 'vue-resource'
 import Home from './pages/home/home.vue';
 import About from './pages/about/about.vue';
 import MainNav from './pages/main-nav/main-nav.vue';
+ 
 import Places from './pages/places/places-center.vue';
-<<<<<<< HEAD
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
 Vue.http.options.root = 'http://localhost:3003';
 
+ 
 
-=======
 import Emails from './pages/email-center/email-center.vue';
 import Events from './pages/event-center/event-center.vue';
 
-Vue.use(VueRouter);
 
->>>>>>> 92cb4b5ba120e8a9024468f948675491ed424516
-const routes = [{
+const routes = [{ 
     path: '/',
     component: Home
 }, {
