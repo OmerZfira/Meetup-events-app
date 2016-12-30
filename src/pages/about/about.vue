c650c543f2893f046ffd5b0ae637fe1df54002d8
hadaskoren <das.koren@gmail.com>
2016-12-30 14:16:37 +0200

    added about

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -1,7 +1,13 @@
 <template>
     <div id="about">
         <div>
-             <h1>About Component</h1>  
+             <br>
+             <div class="images">
+                <img class="image" src="../../assets/adir.jpg"/>
+                <img class="image" src="../../assets/omer.jpg"/>
+                <img class="image" src="../../assets/me.jpg"/>
+             </div>
+             
         </div>   
     </div> 
 </template>
@@ -17,5 +23,15 @@
 </script>
 
 <style scoped>
- 
+    .images {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    .image {
+        border-radius: 50%;
+        width: 200px;
+        height: 200px;
+        border: 3px solid black;
+    }
 </style>
